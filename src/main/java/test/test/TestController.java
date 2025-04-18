19ca3df0e7e88216c96ed95559e1e10e653b75f6
dinal.fourdiv <dinal.fourdiv@gmail.com>
2025-04-18 20:24:27 +0530

    1 commkt

---
+++ src/main/java/test/test/TestController.java
@@ -0,0 +1,13 @@
+package test.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
